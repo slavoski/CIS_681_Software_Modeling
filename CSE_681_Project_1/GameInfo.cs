21e137bba4c05fe98f564d381dc3b93f76a5a51b
slavoski <slavs12@yahoo.com>
2023-02-10 22:29:49 -0700

    Finally Got the spacing right on the Add Screen

--- CSE_681_Project_1/GameInfo.cs
+++ CSE_681_Project_1/GameInfo.cs
@@ -1,17 +1,29 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace CSE_681_Project_1
 {
 	public class GameInfo
 	{
+		[Description("Date")]
 		public DateTime date { get; set; }
+
 		public TeamInfo homeStats { get; set; } = new TeamInfo();
+
+		[Description("Home Team")]
 		public string homeTeamName { get; set; } = "";
+
 		public string HomeVsTeam => homeTeamName + " vs " + visTeamName;
+
+		[Description("Is Final")]
 		public bool isFinal { get; set; } = false;
+
+		[Description("Neutral")]
 		public bool neutral { get; set; } = false;
+
 		public TeamInfo visStats { get; set; } = new TeamInfo();
 
+		[Description("Visitor Team")]
 		public string visTeamName { get; set; } = "";
 	}
 }
--- CSE_681_Project_1/TeamInfo.cs
+++ CSE_681_Project_1/TeamInfo.cs
@@ -19,34 +19,49 @@ namespace CSE_681_Project_1
 		[Description("Game Code")]
 		public ulong gameCode { get; set; }
 
+		[Description("Game Date")]
 		public string gameDate { get; set; } = "";
 
+		[Description("Interceptions Thrown")]
 		public int interceptionsThrown { get; set; }
 
+		[Description("Pass Attempts")]
 		public int passAtt { get; set; }
 
+		[Description("Passes Completed")]
 		public int passComp { get; set; }
 
+		[Description("Pass Yards")]
 		public int passYds { get; set; }
 
+		[Description("Penalties")]
 		public int penalties { get; set; }
 
+		[Description("Penalty Yards")]
 		public int penaltYds { get; set; }
 
+		[Description("Rush Attempts")]
 		public int rushAtt { get; set; }
 
+		[Description("Rush Yards")]
 		public int rushYds { get; set; }
 
+		[Description("Score")]
 		public int score { get; set; }
 
+		[Description("Stat ID Code")]
 		public ulong statIdCode { get; set; }
 
+		[Description("Team Code")]
 		public int teamCode { get; set; }
 
+		[Description("Third Down Conversions")]
 		public int thirdDownConver { get; set; }
 
+		[Description("Third Down Attempts")]
 		public int thridDownAtt { get; set; }
 
+		[Description("Time Possesed")]
 		public int timePoss { get; set; }
 	}
 }
