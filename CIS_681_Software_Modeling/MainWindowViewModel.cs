11386be809298708b80ca841016715843d06050a
slavoski <slavs12@yahoo.com>
2023-03-12 18:36:18 -0600

    hijack first project to do multiprocessing and multithreading.

--- CIS_681_Software_Modeling/MainWindowViewModel.cs
+++ CIS_681_Software_Modeling/MainWindowViewModel.cs
@@ -1,5 +1,11 @@
 ﻿using MvvmHelpers;
 using MvvmHelpers.Commands;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace CIS_681_Software_Modeling
 {
@@ -8,12 +14,15 @@ namespace CIS_681_Software_Modeling
 		#region member variables
 
 		private string _content;
-		private string _defaultContent = "Hello World!";
 		private string _newContent;
+		private int _numberOfProcesses = 2;
+		private int _numberOfThreads = 4;
+		private List<int> _numbers;
+		private List<Thread> _threads = new List<Thread>();
 
 		#endregion member variables
 
-		#region Commands
+		#region Properties
 
 		public string Content
 		{
@@ -25,7 +34,30 @@ namespace CIS_681_Software_Modeling
 			}
 		}
 
-		public Command DisplayOutputCommand
+		public int NumberOfProcesses
+		{
+			get => _numberOfProcesses;
+			set => SetProperty(ref _numberOfProcesses, value);
+		}
+
+		public int NumberOfThreads
+
+		{
+			get => _numberOfThreads;
+			set => SetProperty(ref _numberOfThreads, value);
+		}
+
+		#endregion Properties
+
+		#region Commands
+
+		public Command MultiProcessCommand
+		{
+			get;
+			set;
+		}
+
+		public Command MultiThreadCommand
 		{
 			get;
 			set;
@@ -47,17 +79,94 @@ namespace CIS_681_Software_Modeling
 
 		public MainWindowViewModel()
 		{
-			DisplayOutputCommand = new Command(() => DisplayOutput());
-			Content = NewContent = _defaultContent;
+			MultiProcessCommand = new Command(() => MultiProcess());
+			MultiThreadCommand = new Command(() => MultiThread());
+			_numbers = Enumerable.Range(1, 100000000).ToList();
 		}
 
 		#endregion constructor / destructor
 
 		#region methods
 
-		private void DisplayOutput()
+		private void CalculateNewValue(List<int> currRange)
 		{
-			Content = NewContent;
+			currRange.ForEach(range =>
+			{
+				range *= 12342134;
+				Console.WriteLine(range);
+			});
+		}
+
+		private int CheckValue(int currValue, int maxValue = 10)
+		{
+			if (currValue <= 0)
+			{
+				currValue = 1;
+			}
+			else if (currValue > maxValue)
+			{
+				currValue = maxValue;
+			}
+
+			return currValue;
+		}
+
+		private void InitializeThreads()
+		{
+			var range = _numbers.Count / _numberOfThreads;
+
+			for (int i = 0; i < NumberOfThreads - 1; i++)
+			{
+				_threads.Add(new Thread(() =>
+				{
+					CalculateNewValue(_numbers.GetRange(i * range, range));
+				}));
+			}
+		}
+
+		private async void MultiProcess()
+		{
+			var numOfTotalProcessors = Environment.ProcessorCount;
+
+			NumberOfProcesses = CheckValue(NumberOfProcesses, numOfTotalProcessors);
+			Content = "Running Multi Process";
+
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			await Task.Run(() =>
+			{
+				Parallel.ForEach(_numbers, number =>
+				{
+					number *= 12342134;
+					Console.WriteLine(number);
+				});
+
+				stopwatch.Stop();
+			});
+
+			Content = $"Time to Completion: {stopwatch.Elapsed} ";
+		}
+
+		private async void MultiThread()
+		{
+			NumberOfThreads = CheckValue(NumberOfThreads, 100);
+			InitializeThreads();
+			Content = "Running Multi Threading";
+
+			Stopwatch stopwatch = Stopwatch.StartNew();
+
+			await Task.Run(() =>
+			{
+				_threads.ForEach(thread => { thread.Start(); });
+				_threads.ForEach(thread => { thread.Join(); });
+
+				stopwatch.Stop();
+			});
+
+			Content = $"Time to Completion: {stopwatch.Elapsed} ";
+
+			_threads.ForEach(thread => thread = null);
+			_threads.Clear();
 		}
 
 		#endregion methods
